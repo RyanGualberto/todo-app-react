0b988c3534250ee095af946229c55483ff03ad5e
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-22 09:38:38 -0300

    s8a145: conditional render

--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -1,19 +1,19 @@
 import React from "react";
+import Grid from "../template/grid";
+import IconButton from "../template/iconButton";
 
 export default props => (
     <div role='form' className="todoForm">
-        <div className="col-xs-12 col-sm-9 col-md-10">
+        <Grid cols="12 9 10">
             <input 
                 type="text" 
                 id="description" 
                 className="form-control" 
                 placeholder="Adicione Uma Tarefa" />
-        </div>
+        </Grid>
 
-        <div className="col-xs-12 col-sm-3 col-md-2">
-            <button className="btn btn-primary">
-                +
-            </button>
-        </div>
+        <Grid cols="12 3 2">
+            <IconButton style='primary' icon="plus"/>
+        </Grid  >
     </div>
 )
