d27b4d6850df83a00347e3a7c2a00c8c7f8a725e
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-22 09:10:36 -0300

    s8141: create form and list components

--- front-end/src/todo/todo.jsx
+++ front-end/src/todo/todo.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import PageHeader from "../template/pageHeader";
+import TodoForm from "./todoForm";
+import TodoList from "./todoList";
 
 export default class Todo extends Component {
     render(){
         return (
             <div>
                 <PageHeader nome='Tarefas' texto="Cadastro"></PageHeader>
+                <TodoForm/>
+                <TodoList/>
             </div>
         )
     }
---
+++ front-end/src/todo/todoForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default props => (
+    <div>
+        <h1>form</h1>
+    </div>
+)
