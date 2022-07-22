4d3f58208f8fb618f8a23525fe9c1debcafdf6cf
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-22 12:55:02 -0300

    s6a153: create shortkey and finish project

--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -2,27 +2,38 @@ import React from "react";
 import Grid from "../template/grid";
 import IconButton from "../template/iconButton";
 
-export default props => (
-    <div role='form' className="todoForm">
-        <Grid cols="12 9 10">
-            <input 
-                type="text" 
-                id="description" 
-                className="form-control" 
-                placeholder="Adicione Uma Tarefa"
-                onChange={props.handleChange}
-                value={props.description}
-                />
-        </Grid>
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key === 'Enter'){
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-        <Grid cols="12 3 2">
-            <IconButton 
-                style='primary' 
-                icon="plus"
-                onClick={props.handleAdd}
-            />
-            <IconButton style="info" icon="search" onClick={props.handleSearch} />
-            <IconButton style="default" icon="close" onClick={props.handleClear} />
-        </Grid  >
-    </div>
-)
+    return (
+        <div role='form' className="todoForm">
+            <Grid cols="12 9 10">
+                <input 
+                    type="text" 
+                    id="description" 
+                    className="form-control" 
+                    placeholder="Adicione Uma Tarefa"
+                    onChange={props.handleChange}
+                    onKeyUp={keyHandler}
+                    value={props.description}
+                    />
+            </Grid>
+    
+            <Grid cols="12 3 2">
+                <IconButton 
+                    style='primary' 
+                    icon="plus"
+                    onClick={props.handleAdd}
+                />
+                <IconButton style="info" icon="search" onClick={props.handleSearch} />
+                <IconButton style="default" icon="close" onClick={props.handleClear} />
+            </Grid  >
+        </div>
+    )
+}
