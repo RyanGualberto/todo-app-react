ba1d9e67524b26421b939beffbc33d294d4ba515
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-22 09:17:31 -0300

    s8a143:  structing form

--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 export default props => (
-    <div>
-        <h1>form</h1>
+    <div role='form' className="todoForm">
+        <div className="col-xs-12 col-sm-9 col-md-10">
+            <input 
+                type="text" 
+                id="description" 
+                className="form-control" 
+                placeholder="Adicione Uma Tarefa" />
+        </div>
+
+        <div className="col-xs-12 col-sm-3 col-md-2">
+            <button className="btn btn-primary">
+                +
+            </button>
+        </div>
     </div>
 )
