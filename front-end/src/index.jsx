b0b199c3cf9d9753d0c4acc6a5a3fe82a15835a5
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-25 10:14:43 -0300

    s10a175: config redux

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -3,5 +3,15 @@ import ReactDOM  from "react-dom";
 import App from "./main/app";
 import 'modules/bootstrap/dist/css/bootstrap.min.css'
 import 'modules/font-awesome/css/font-awesome.min.css'
+import { createStore} from 'redux'
+import { Provider } from 'react-redux'
+import reducers from './main/reducers'
 
-ReactDOM.render(<App />, document.getElementById('app'))
+const store = createStore(reducers)
+
+ReactDOM.render(
+    <Provider store={store}>  
+        <App />
+    </Provider>
+    , document.getElementById('app')
+)
