5d0c59abf74312da6428164f508986f654ccd820
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-25 11:05:39 -0300

    s10a182: Add todo

--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -2,7 +2,7 @@ import React, {Component} from "react";
 import Grid from "../template/grid";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
-import { changeDescription, search } from "./todoActions";
+import { add, changeDescription, search } from "./todoActions";
 import IconButton from "../template/iconButton";
 
 class TodoForm extends Component {
@@ -16,14 +16,16 @@ class TodoForm extends Component {
     }
 
     keyHandler(e){
+        const {add, search, description} = this.props
         if(e.key === 'Enter'){
-            e.shiftKey ? this.props.handleSearch() : this.props.handleAdd()
+            e.shiftKey ? search() : add(description)
         } else if (e.key === 'Escape') {
             props.handleClear()
         }
     }
 
     render(){
+        const {add, search, description} = this.props
         return (
             <div role='form' className="todoForm">
             <Grid cols="12 9 10">
@@ -42,9 +44,9 @@ class TodoForm extends Component {
                 <IconButton 
                     style='primary' 
                     icon="plus"
-                    onClick={this.props.handleAdd}
+                    onClick={() => add(description)}
                 />
-                <IconButton style="info" icon="search" onClick={this.props.handleSearch} />
+                <IconButton style="info" icon="search" onClick={() => search()} />
                 <IconButton style="default" icon="close" onClick={this.props.handleClear} />
             </Grid  >
         </div>
@@ -59,6 +61,7 @@ function mapStateToProps(state){
 }
 const mapDispatchToProps = dispatch =>  
     bindActionCreators({
+        add,
         changeDescription,
         search
     }, 
--- front-end/src/todo/todoReducer.js
+++ front-end/src/todo/todoReducer.js
@@ -9,6 +9,8 @@ const INITIAL_STATE = {
                 return{...state, description: action.payload}
             case 'TODO_SEARCHED':
                 return{...state, list: action.payload.data}
+            case 'TODO_ADDED':
+                return {...state, description: ''}
             default:
                 return state
         }
