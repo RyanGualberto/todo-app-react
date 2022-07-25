c45555d452d3acc493223be2744a05a35b0c6a5b
Ryan <ryanoliveiragualberto@gmail.com>
2022-07-25 11:10:24 -0300

    s10a184: apply middleware(redux-multi)

--- front-end/src/index.jsx
+++ front-end/src/index.jsx
@@ -6,10 +6,11 @@ import 'modules/font-awesome/css/font-awesome.min.css'
 import { applyMiddleware ,createStore} from 'redux'
 import { Provider } from 'react-redux'
 import promise from "redux-promise";
+import multi from 'redux-multi'
 import reducers from './main/reducers'
 
 const devTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-const store = applyMiddleware(promise)(createStore)(reducers, devTools)
+const store = applyMiddleware(multi, promise)(createStore)(reducers, devTools)
 
 ReactDOM.render(
     <Provider store={store}>  
--- front-end/src/todo/todoForm.jsx
+++ front-end/src/todo/todoForm.jsx
@@ -46,7 +46,7 @@ class TodoForm extends Component {
                     icon="plus"
                     onClick={() => add(description)}
                 />
-                <IconButton style="info" icon="search" onClick={() => search()} />
+                <IconButton style="info" icon="search" onClick={() => search( )} />
                 <IconButton style="default" icon="close" onClick={this.props.handleClear} />
             </Grid  >
         </div>
